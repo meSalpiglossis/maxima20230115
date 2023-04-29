799ec8081e2464d3d7cc6bcf014c47769a68b847
meSalpiglossis <125427233+meSalpiglossis@users.noreply.github.com>
2023-04-29 18:55:11 +1000

    Nested frames minor code edits

--- src/test/java/tests/base/NestedFramesTestBase.java
+++ src/test/java/tests/base/NestedFramesTestBase.java
@@ -3,6 +3,7 @@ package tests.base;
 import org.testng.annotations.BeforeMethod;
 
 import objects.pages.frames.NestedFramesPage;
+import org.testng.asserts.SoftAssert;
 
 import static common.WebBrowser.openLink;
 
@@ -10,8 +11,12 @@ public class NestedFramesTestBase extends AllTestsBase{
 
     protected NestedFramesPage nestedFramesPage = new NestedFramesPage();
 
+    protected SoftAssert soft;
+
     @BeforeMethod
     protected void openLinkInWebBrowser() {
         openLink("https://the-internet.herokuapp.com/nested_frames");
+
+        soft = new SoftAssert();
     }
 }

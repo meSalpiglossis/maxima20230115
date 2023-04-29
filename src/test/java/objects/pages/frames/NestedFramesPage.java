cb1cf72539c4f5e5a4dd6e9f7ff30f438321b16b
meSalpiglossis <125427233+meSalpiglossis@users.noreply.github.com>
2023-04-29 15:56:18 +1000

    Nested frames minor code edits

--- src/test/java/objects/pages/frames/NestedFramesPage.java
+++ src/test/java/objects/pages/frames/NestedFramesPage.java
@@ -8,46 +8,53 @@ import static common.WebBrowser.webBrowser;
 public class NestedFramesPage extends PageBase {
 
     public NestedFramesPage switchToLeftFrame() {
-        webBrowser().switchTo().defaultContent();
-        webBrowser().switchTo().frame(FRAME_TOP);
-        webBrowser().switchTo().frame(FRAME_LEFT);
+        webBrowser()
+            .switchTo().defaultContent()
+            .switchTo().frame(FRAME_TOP)
+            .switchTo().frame(FRAME_LEFT);
         return this;
     }
 
     public boolean isLeftFrameTextMatchExpected() {
-        return find(BODY).getText().equals(FRAME_LEFT_EXPECTED_TEXT);
+        return isTextInBodyMatchExpexted(FRAME_LEFT_EXPECTED_TEXT);
     }
 
     public NestedFramesPage switchToMiddleFrame() {
-        webBrowser().switchTo().defaultContent();
-        webBrowser().switchTo().frame(FRAME_TOP);
-        webBrowser().switchTo().frame(FRAME_MIDDLE);
+        webBrowser()
+            .switchTo().defaultContent()
+            .switchTo().frame(FRAME_TOP)
+            .switchTo().frame(FRAME_MIDDLE);
         return this;
     }
 
     public boolean isMiddleFrameTextMatchExpected() {
-        return find(BODY).getText().equals(FRAME_MIDDLE_EXPECTED_TEXT);
+        return isTextInBodyMatchExpexted(FRAME_MIDDLE_EXPECTED_TEXT);
     }
 
     public NestedFramesPage switchToRightFrame() {
-        webBrowser().switchTo().defaultContent();
-        webBrowser().switchTo().frame(FRAME_TOP);
-        webBrowser().switchTo().frame(FRAME_RIGHT);
+        webBrowser()
+            .switchTo().defaultContent()
+            .switchTo().frame(FRAME_TOP)
+            .switchTo().frame(FRAME_RIGHT);
         return this;
     }
 
     public boolean isRightFrameTextMatchExpected() {
-        return find(BODY).getText().equals(FRAME_RIGHT_EXPECTED_TEXT);
+        return isTextInBodyMatchExpexted(FRAME_RIGHT_EXPECTED_TEXT);
     }
 
     public NestedFramesPage switchToBottomFrame() {
-        webBrowser().switchTo().defaultContent();
-//        webBrowser().switchTo().frame(FRAME_TOP);
-        webBrowser().switchTo().frame(FRAME_BOTTOM);
+        webBrowser()
+            .switchTo().defaultContent()
+            .switchTo().frame(FRAME_BOTTOM);
         return this;
     }
 
     public boolean isBottomFrameTextMatchExpected() {
-        return find(BODY).getText().equals(FRAME_BOTTOM_EXPECTED_TEXT);
+        return isTextInBodyMatchExpexted(FRAME_BOTTOM_EXPECTED_TEXT);
+    }
+
+    private boolean isTextInBodyMatchExpexted(String expectedText) {
+        return find(BODY).getText().equals(expectedText);
     }
 }

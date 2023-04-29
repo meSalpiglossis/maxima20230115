286b880c09518cd51641873ae0a080a23e180150
meSalpiglossis <125427233+meSalpiglossis@users.noreply.github.com>
2023-04-29 18:56:23 +1000

    iFrame test

---
+++ src/test/java/objects/pages/frames/IFrameLocators.java
@@ -0,0 +1,12 @@
+package objects.pages.frames;
+
+import org.openqa.selenium.By;
+
+public class IFrameLocators {
+    public static final String EXPECTED_TEXT = "EXPECTED TEXT";
+    public static final String ATTRIBUTE_INNER_TEXT = "innerText";
+
+    public static final String TINY_MCE_FRAME = "mce_0_ifr";
+
+    public static final By INPUT_FIELD = By.id("tinymce");
+}
--- src/test/java/tests/base/IFrameTestBase.java
+++ src/test/java/tests/base/IFrameTestBase.java
@@ -1,15 +1,21 @@
 package tests.base;
 
-import objects.pages.frames.IFramePage;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import objects.pages.frames.IFramePage;
+
+import static common.WebBrowser.closeWebBrowser;
 import static common.WebBrowser.openLink;
 
-public class IFrameTestBase {
+public class IFrameTestBase extends AllTestsBase {
+
     protected IFramePage iFramePage = new IFramePage();
 
     @BeforeMethod
     protected void openLinkInWebBrowser() {
         openLink("https://the-internet.herokuapp.com/iframe");
     }
+
+
 }

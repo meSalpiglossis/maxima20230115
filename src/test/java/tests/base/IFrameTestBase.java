6f96a801bcbf32d17df388e7ea8bc6e272f0d789
meSalpiglossis <125427233+meSalpiglossis@users.noreply.github.com>
2023-04-29 22:37:38 +1000

    minor code edits

--- src/test/java/tests/base/IFrameTestBase.java
+++ src/test/java/tests/base/IFrameTestBase.java
@@ -1,11 +1,9 @@
 package tests.base;
 
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 import objects.pages.frames.IFramePage;
 
-import static common.WebBrowser.closeWebBrowser;
 import static common.WebBrowser.openLink;
 
 public class IFrameTestBase extends AllTestsBase {
